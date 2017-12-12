0ccdd3d40025425044d36d4d5e459d079768809d
Maydoh1239 <edlam1889@gmail.com>
2017-12-11 16:25:36 -0800

    start js

--- JS/punnett-square.js
+++ JS/punnett-square.js
@@ -1,9 +1,57 @@
 var canvas = document.getElementById('out');
-var input = document.getElementById('input').value;
+var ctx = canvas.getContext("2d");
+var sqsize = canvas.width;
+var input1;
+var input2;
+console.log(sqsize);
+
+function update(){
+  sqsize = canvas.width
+  input1 = document.getElementById('input1').value;
+  input2 = document.getElementById('input2').value;
+  console.log(input1);
+  console.log(input2);
+  if (input1.length===input2.length && input1.length/2==Math.round(input1.length/2)){
+    run();
+  }
+}
+
+function run(){
+  var bins=[];
+
+}
 
 function getColor(s){
-  var i;
   for(i=0;i<s.len;i+=2){
-    
+
   }
+}
+
+function hslhex(h, s, l) {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
--- Python/Squares.py
+++ Python/Squares.py
@@ -82,7 +82,7 @@ while True:
             else:
                 p=''.join(sorted(r[j-1]+c[i-1], key=lambda s: ord(s.lower())*2+(1 if s.lower()==s else 0)))
                 q = getcolor(p)
-                d.rectangle((i*(sqsize+1)+1,j*(sqsize+1)+1,i*(sqsize+1)+sqsize,j*(sqsize+1)++sqsize),fill=q)
+                d.rectangle((i*(sqsize+1)+1,j*(sqsize+1)+1,i*(sqsize+1)+sqsize,j*(sqsize+1)+sqsize),fill=q)
                 d.text((i*(sqsize+1)+1+3*mult,j*(sqsize+1)+1+sqsize/2-5*mult),p,fill=(0,0,0),font=ImageFont.truetype("/Library/Fonts/Courier New.ttf", 10*mult),spacing=0)
 
     minicap=[7424, 665, 7428, 7429, 7431, 1171, 610, 668, 618, 7434, 7435, 671, 7437, 628, 7439, 7448, 491, 640, 115, 7451, 7452, 7456, 7457, 120, 655, 7458]

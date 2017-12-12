37c6a003adaea0a9e43ffc380a914074a66e42ed
Maydoh1239 <edlam1889@gmail.com>
2017-12-11 18:07:55 -0800

    change size

--- JS/letters/0_letters.py
+++ JS/letters/0_letters.py
@@ -4,6 +4,7 @@ import os
 location = os.path.dirname(os.path.abspath(__file__))
 
 for a in string.ascii_lowercase+string.ascii_uppercase:
-    im = Image.new('RGBA',(6,10))
-    ImageDraw.Draw(im).text((0,0),a,fill=(0,0,0),font=ImageFont.truetype("/Library/Fonts/Courier New.ttf", 10),spacing=0)
+    mult=25
+    im = Image.new('RGBA',(6*mult,10*mult))
+    ImageDraw.Draw(im).text((0,0),a,fill=(0,0,0),font=ImageFont.truetype("/Library/Fonts/Courier New.ttf", 10*mult),spacing=0)
     im.save(location+"/"+('1' if a ==a.upper() else '0')+a+".png")

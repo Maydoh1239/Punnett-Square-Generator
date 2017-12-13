594da63772997515b6fcb84e1af458083c3a5b85
Maydoh1239 <edlam1889@gmail.com>
2017-12-13 14:33:06 -0800

    Add zooming

--- JS/punnett-square.js
+++ JS/punnett-square.js
@@ -1,15 +1,70 @@
 var canvas = document.getElementById('out');
 var ctx = canvas.getContext("2d");
 var sqsize = canvas.width;
+var sqnum = null;
 var input1;
 var input2;
+var sqX = 0;
+var sqY = 0;
+var mouseX = null;
+var mouseY = null;
+var mouseDown = false;
+var loadDone = false;
+var square = null;
+var currId = 0;
+
+canvas.onmousewheel = function (evt) {
+  evt.preventDefault();
+  var scale = (sqnum*sqsize)/canvas.width;
+  var wheel = Math.min(500,Math.max(-499,evt.wheelDelta))/500+1;
+  if ( (scale<=1 && scale*wheel>scale) || scale>1 ){
+    scaleChange = scale*wheel-scale;
+    sqX -= scaleChange*(mouseX-sqX)/scale;
+    sqY -= scaleChange*(mouseY-sqY)/scale;
+    scale*=wheel;
+    sqsize = canvas.width*scale/sqnum;
+    renderFrame(sqX,sqY,sqsize);
+  }
+}
+
+document.addEventListener ( 'mousemove' , getMouseCoords);
+document.addEventListener ( 'mouseenter' , getMouseCoords);
+
+document.addEventListener ( 'mousedown' , function ( evt ) {
+  mouseDown = true;
+});
+
+document.addEventListener ( 'mouseup' , function ( evt ) {
+  mouseDown = false;
+});
+
+function getMouseCoords ( evt ) {
+  var rect = canvas.getBoundingClientRect();
+  if ( mouseDown && mouseX > 0 && mouseY > 0 && mouseX < canvas.width && mouseY < canvas.height) {
+    sqX -= (mouseX - (evt.clientX - rect.left));
+    sqY -= (mouseY - (evt.clientY - rect.top));
+    renderFrame(sqX,sqY,sqsize);
+  }
+  mouseX = (evt.clientX - rect.left);
+  mouseY = (evt.clientY - rect.top);
+}
+
+function mouseX() {
+  return mouseX;
+}
+
+function mouseY() {
+  return mouseY;
+}
 
 function update(){
-  sqsize = canvas.width
+  loadDone = false;
+  sqX = 0;
+  sqY = 0;
   input1 = document.getElementById('input1').value;
   input2 = document.getElementById('input2').value;
-  //Checks if both parents have same number of alleles and whether it is an even amount total
-  if (input1.length===input2.length && input1.length/2==Math.round(input1.length/2)){
+  //Checks if both parents have same number of alleles and whether it is an even amount total and neither is blank
+  if (input1.length===input2.length && input1.length>0 && input1.length/2==Math.round(input1.length/2)){
     run();
   }
 }
@@ -37,51 +92,83 @@ function run(){
     c.push(s1);
     r.push(s2);
   }
-  addText("test", 100,10,10);
-  addText("best", 100, 500,500);
 
-}
+  sqnum = c.length+1;
 
-function renderFrame(x1, y1, x1, y1){
+  square = new Array ( c.length ); // the actual punnett square
 
-}
-
-function addText(string , size , x , y){
-  for ( var i = 0; i < string.length; i++ ){
-    var letter = new Image();
-    letter.src = "letters/"+string.charCodeAt(i)+".png";
-    console.log(i)
-    console.log("loadimagehere")
-    letter.onload = function(){
-      console.log(i)
-      console.log(x+(size*i*0.6)+","+y)
-      ctx.drawImage( letter ,x+size*i*0.6 , y , size*0.6, size);
+  for ( i = 0; i <= c.length; i++ ){
+    if ( i>0 ){
+      square[i-1] = new Array ( r.length );
     }
-    console.log(letter.complete); //wait until image loads
+    for ( j = 0; j <= r.length; j++){
+      if ( i != 0 && j != 0 ) {
+        square[i-1][j-1] = geneSort(c[i-1]+r[j-1]);
+      }
+    }
   }
+  loadDone = true;
+  sqsize = canvas.width/(square.length+1);
+  renderFrame(0,0,sqsize)
+}
+
+function renderFrame(x1, y1, sSize){ //allows zooming
+  currId++;
+  if ( currId == 10000 ) {
+    currId %= 10000;
+  }
+  if ( loadDone ) { //Done loading square?
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for (var a = 0; a < square.length; a++) {
+      for (var b = 0; b < square[a].length; b++){
+        shouldRender(x1,y1,sSize,a,b);
+      }
+    }
+  }
+}
+
+function shouldRender ( x1 , y1 , sSize , a , b ) { //should this be rendered?
+  if ( sSize*(a+2)+x1 > 0 && sSize*(b+2)+y1 > 0 && sSize*(a+1)+x1 < canvas.width && sSize*(b+1)+y1 < canvas.height ) {
+    ctx.fillStyle = getColor(square[a][b]);
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect (sSize*(a+1)+x1,sSize*(b+1)+y1,sSize,sSize);
+    if ( sqsize > 20 ){
+      addText (square[a][b], sSize*0.95, sSize*(a+1.5)+x1,sSize*(b+1.5)+y1, currId);
+    }
+  }
+}
+
+function addText(string , size , x , y , id){ // Loads a word
+  x = x-size*0.3;
+  size/=string.length;
+  ctx.fillStyle = '#000000';
+  ctx.font = Math.round(size)+'px Courier'
+  ctx.fillText(string,x,y);
 }
 
 function geneSort ( s ){
   s1 = s.split('');
-  function compare ( a , b ){
+  return s1.sort(function ( a , b ){ // sorts by alleles
     var a1 = a.toLowerCase().charCodeAt(0)*2+(a.toLowerCase()==a ? 1 : 0);
-    var b1 = b.toLowerCase().charCodeAt(0)*2+(b.toLowerCase()==a ? 1 : 0)
-    return a - b;
-  }
-  return s1.sort(compare()).join('');
+    var b1 = b.toLowerCase().charCodeAt(0)*2+(b.toLowerCase()==b ? 1 : 0);
+    return a1 - b1;
+  }).join('');
 }
 
-function getColor(s){
-  for(i=0;i<s.len;i+=2){
-
+function getColor( s ){
+  var sum = 0;
+  for(i=0;i<s.length;i+=2){
+    var chr = s.charAt(s.length-i-2);
+    if (chr == chr.toUpperCase()){
+      sum += Math.pow(2,i/2);
+    }
   }
+  return hslhex(sum/(sqnum-1) , 1 , 0.5);
 }
 
 function hslhex(h, s, l) {
   //Converts HSL to Hex
-  h /= 360;
-  s /= 100;
-  l /= 100;
+  var h,s,l;
   let r, g, b;
   if (s === 0) {
     r = g = b = l; // achromatic
